9887c9b0a84a169cbf1afc2c9e2ce06d3a644cd3
AleksNau <workciti@yandex.ru>
2024-01-13 16:57:09 +0300

    logo + search

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,10 @@
-import "./Logo.module.scss";
+import s from "./Logo.module.scss";
+import logo from "./images/logo.svg";
 
-const Logo = () => <div>Logo</div>;
+const Logo = () => (
+  <a href="/">
+    <img className={s.img} src={logo} alt="Логотип мебельного магазина Koff" />
+  </a>
+);
 
 export default Logo;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,15 @@
-import "./Navigation.module.scss";
+import s from "./Navigation.module.scss";
 
-const Navigation = () => <div>Navigation</div>;
+const Navigation = () => (
+  <nav className={s.navigation}>
+    <a className={s.link} href="/favorite">
+      <span className={s.text}></span>
+    </a>
+    <a className={s.link} href="/cart">
+      <span className={s.text}></span>
+      <span>(0)</span>
+    </a>
+  </nav>
+);
 
 export default Navigation;
