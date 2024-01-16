40c9d8c96917e2f88e87291e16ce94a7a02da762
AleksNau <workciti@yandex.ru>
2024-01-16 10:57:11 +0300

    grid card

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,5 +1,11 @@
 import s from "./CardItem.module.scss";
+import chair from "./images/chair.png"
 
-const CardItem = () => <article className={s.card}>Диван</article>;
+const CardItem = () => (<article className={s.card}>
+    <img className={s.image} src={chair} alt="Изображение товара"/>
+    <p className={s.text}>Кресло с подлокотниками</p>
+    <p className={s.price}>5 000 ₽</p>
+    <button className={s.button}>В корзину</button>
+</article>);
 
 export default CardItem;
