1e46dec6227aa2664fb44a14076b69455376fd99
AleksNau <workciti@yandex.ru>
2024-01-28 21:21:44 +0300

    redux

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth/auth.slice.js";
+import categoriesReducer from "./categories/categories.slice.js";
+
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    categories: categoriesReducer,
+  },
+});
--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,10 +1,23 @@
 import "./Main.module.scss";
 import Goods from "../../components/Goods/Goods.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchAccessToken } from "../../store/auth/auth.slice.js";
+import { fetchCategoreis } from "../../store/categories/categories.slice.js";
 
-const Main = () => (
+const Main = () => {
+  const dispatch = useDispatch();
+  const { data: dataCategories, loading: loadingCategories, error: errorCategories } = useSelector((state) => state.categories());
+
+  useEffect(() => {
+    if (!accessToken) {
+      dispatch(fetchCategoreis());
+    }
+  }, [dispatch]);
+  return (
   <main className="main">
-    {/* <Catalog /> */}
+    {/* <Catalog data={dataCategories} /> */}
     <Goods />
   </main>
-);
+)}
 export default Main;
